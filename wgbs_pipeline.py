de7e64d0b767d27d79c96d9c28b5692e95085aa6
PavelBashtrykov <pavel.bashtrykov@gmail.com>
2022-07-25 15:25:57 +0200

    Update wgbs_pipeline.py
    path of the intallation dir

--- wgbs_pipeline.py
+++ wgbs_pipeline.py
@@ -14,10 +14,11 @@ def main():
     Pipeline to analyse whole-genome bisulfite sequencing data.
     List of SRA ids and commands are written in the config.json file, which has to be in the same working directory.
     """
-
     # Load configuration
+    rep_path = os.path.dirname(__file__)
+
     config_file = "config.json"
-    conf_file_path = os.path.join(os.path.dirname(__file__), config_file)
+    conf_file_path = os.path.join(rep_path, config_file)
     conf = json.load(open(file=conf_file_path))
 
     # Get SRA ids
@@ -28,7 +29,7 @@ def main():
 
     for f in ids:
         for c in commands:
-            command = c.format(tag=f)
+            command = c.format(tag=f, path=rep_path)
             print(f"Starting command: {command}")
             output = os.popen(command)
             for line in output:
